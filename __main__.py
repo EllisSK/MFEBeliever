63aeb5bdc7d4a778949e4939f00f18d83936a191
EllisSK <ellissinclairkent@gmail.com>
2025-06-09 05:11:18 +0100

    First commit!
    This commit is the first of the project and adds the __main__.py file containing core imports and a message explaining how the file should be structured.

---
+++ __main__.py
@@ -0,0 +1,9 @@
+import machine as m
+import utime as t
+import _thread as mt
+
+"""
+This is the main file which will control the drone! 
+Other modules should create objects for different sensors and classes with methods for automated flight functions.
+This file should contain two loops running simultaneously, one to handle basic flight control and the other to handle autopilot and sensor arithmetic.
+"""
